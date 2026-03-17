3a218b812d78b9ceb89a64232f37544fa2d4b4c1
akarauser <>
2026-03-17 19:11:00 +0200

    feat: add sign up and sign in system with user specific databases

--- adaptor/main.py
+++ adaptor/main.py
@@ -9,6 +9,7 @@ from typing import Any
 import streamlit as st
 from ollama import ChatResponse, Client
 from PIL import Image
+from scripts.user import check_username, init_db_user, signup_user, singin_user
 from scripts.utils import config_args, logger
 
 st.set_page_config(
@@ -17,6 +18,41 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+
+# Initial folder creation
+if not os.path.exists("adaptor/data/history"):
+    os.makedirs("adaptor/data/history")
+
+init_db_user()
+
+# Session States
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+if "session" not in st.session_state:
+    st.session_state["session"] = datetime.now().strftime("%y-%m-%d_%H-%M-%S")
+
+if "chosen_session" not in st.session_state:
+    st.session_state["chosen_session"] = ""
+
+if "initial" not in st.session_state:
+    st.session_state["initial"] = True
+
+if "image_uploaded" not in st.session_state:
+    st.session_state["image_uploaded"] = False
+
+if "signedin" not in st.session_state:
+    st.session_state["signedin"] = False
+
+if "username" not in st.session_state:
+    st.session_state["username"] = None
+
+if "username_exist" not in st.session_state:
+    st.session_state["username_exist"] = False
+
+if "wrong_information" not in st.session_state:
+    st.session_state["wrong_information"] = False
 
 
 # Functions
@@ -54,10 +90,11 @@ def conversation(
 def connect_to_db():
     """Initialize connection with conversation database"""
     try:
-        return sqlite3.connect("adaptor/data/history/conversations.db")
-    except Exception:
-        # logger.warning(f"Failed to connect db: {e}")
-        print("Failed")
+        return sqlite3.connect(
+            f"adaptor/data/history/{st.session_state['username']}_conversations.db"
+        )
+    except Exception as e:
+        logger.warning(f"Failed to connect db: {e}")
 
 
 def init_db():
@@ -159,89 +196,126 @@ def load_captions() -> list:
     return captions_list
 
 
-# Session States
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+# Login dialog
+@st.dialog("Welcome to Adaptor", dismissible=False)
+def signup_signin():
+    global USER
+    signup_tab, signin_tab = st.tabs(["Sign Up", "Sign In"])
 
-if "session" not in st.session_state:
-    st.session_state["session"] = datetime.now().strftime("%y-%m-%d_%H-%M-%S")
-
-if "chosen_session" not in st.session_state:
-    st.session_state["chosen_session"] = ""
-
-if "initial" not in st.session_state:
-    st.session_state["initial"] = True
-
-if "image_uploaded" not in st.session_state:
-    st.session_state["image_uploaded"] = False
-
-# Initial folder creation
-if not os.path.exists("adaptor/data/history"):
-    os.makedirs("adaptor/data/history")
-
-if st.session_state["initial"]:
-    init_db()
-    start_conversation(st.session_state["session"])
-    st.session_state["initial"] = False
-
-# Sidebar
-with st.sidebar:
-    with st.container(height=240):
-        image_byte = st.file_uploader(
-            "Choose an image for chat",
-            type=["png", "jpg", "jpeg"],
-            help="You can choose an image to interact over chat.",
+    with signup_tab:
+        if st.session_state["username_exist"]:
+            st.error("Username already exist")
+        signup_username = st.text_input("Username", key="signup_username")
+        signup_password = st.text_input(
+            "Password", key="signup_password", type="password"
         )
-        if image_byte:
-            image = Image.open(image_byte)
-            image.save("adaptor/data/image.jpg")
-            st.session_state["image_uploaded"] = True
-    with st.container(height=380):
-        st.session_state["chosen_session"] = st.radio(
-            "Chat History",
-            [conv["session_id"] for conv in get_recent_conversations()],
-            width="stretch",
-            help="Sorting is based on date and time (year-month-day_hour-minute-second). If you do not receive any answer, make sure to repeat your prompt.",
-            captions=load_captions(),
+        if st.button("Sign Up"):
+            if signup_username and signup_password:
+                if check_username(signup_username):
+                    signup_user(signup_username, signup_password)
+                    st.session_state["signedin"] = True
+                    st.session_state["username"] = signup_username
+                    st.rerun()
+                else:
+                    st.session_state["signedin"] = False
+                    st.session_state["username_exist"] = True
+                    st.rerun()
+            else:
+                st.write("Please fill both fields.")
+                st.session_state["signedin"] = False
+                st.rerun()
+
+    with signin_tab:
+        if st.session_state["wrong_information"]:
+            st.error("Please check username and password")
+        signin_username = st.text_input("Username", key="signin_username")
+        signin_password = st.text_input(
+            "Password", key="signin_password", type="password"
         )
+        if st.button("Sign In"):
+            if signin_username and signin_password:
+                if singin_user(signin_username, signin_password):
+                    st.session_state["signedin"] = True
+                    st.session_state["username"] = signin_username
+                    st.rerun()
+                else:
+                    logger.debug("Signin failed")
+                    st.session_state["signedin"] = False
+                    st.session_state["wrong_information"] = True
+                    st.rerun()
+            else:
+                st.write("Please fill both fields.")
+                st.session_state["signedin"] = False
+                st.rerun()
 
-# Chat interface
-for message in get_conversation(st.session_state["chosen_session"]):
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
 
-if prompt := st.chat_input("Type here..."):
-    # User
-    with st.chat_message("user"):
-        st.write(prompt)
-        st.session_state["messages"].append({"role": "user", "content": prompt})
+if not st.session_state["signedin"]:
+    signup_signin()
 
-    # Assistant
-    with st.chat_message("assistant"):
-        thinking_content = ""
-        response_content = ""
-        with st.status("Thinking...", expanded=False) as status:
-            thinking_placeholder = st.empty()
-        response_placeholder = st.empty()
-        stream = conversation(
-            prompt,
-            use_image=st.session_state["image_uploaded"],
-            image_path="adaptor/data/image.jpg",
-            history=get_conversation(st.session_state["chosen_session"]),
-        )
-        if stream:
-            for chunk in stream:
-                if chunk.message.thinking:
-                    thinking_content += chunk.message.thinking
-                    thinking_placeholder.markdown(thinking_content)
-                elif chunk.message.content:
-                    response_content += chunk.message.content
-                    response_placeholder.markdown(response_content)
-            st.session_state["messages"].append(
-                {
-                    "role": "assistant",
-                    "content": response_content,
-                    "thinking": thinking_content,
-                },
+else:
+    if st.session_state["initial"]:
+        init_db()
+        start_conversation(st.session_state["session"])
+        st.session_state["initial"] = False
+
+    # Sidebar
+    with st.sidebar:
+        with st.container(height=220):
+            image_byte = st.file_uploader(
+                "Choose an image for chat",
+                type=["png", "jpg", "jpeg"],
+                help="You can choose an image to interact over chat. Image can be 'png', 'jpg', 'jpeg'.",
             )
-    add_message(st.session_state["chosen_session"], st.session_state["messages"])
+            if image_byte:
+                image = Image.open(image_byte)
+                image.save("adaptor/data/image.jpg")
+                st.session_state["image_uploaded"] = True
+        with st.container(height=320):
+            st.session_state["chosen_session"] = st.radio(
+                "Chat History",
+                [conv["session_id"] for conv in get_recent_conversations()],
+                width="stretch",
+                help="Sorting is based on date and time (year-month-day_hour-minute-second). If you do not receive any answer, make sure to repeat your prompt.",
+                captions=load_captions(),
+            )
+
+    # Chat interface
+    for message in get_conversation(st.session_state["chosen_session"]):
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input("Type here..."):
+        # User
+        with st.chat_message("user"):
+            st.write(prompt)
+            st.session_state["messages"].append({"role": "user", "content": prompt})
+
+        # Assistant
+        with st.chat_message("assistant"):
+            thinking_content = ""
+            response_content = ""
+            with st.status("Thinking...", expanded=False) as status:
+                thinking_placeholder = st.empty()
+            response_placeholder = st.empty()
+            stream = conversation(
+                prompt,
+                use_image=st.session_state["image_uploaded"],
+                image_path="adaptor/data/image.jpg",
+                history=get_conversation(st.session_state["chosen_session"]),
+            )
+            if stream:
+                for chunk in stream:
+                    if chunk.message.thinking:
+                        thinking_content += chunk.message.thinking
+                        thinking_placeholder.markdown(thinking_content)
+                    elif chunk.message.content:
+                        response_content += chunk.message.content
+                        response_placeholder.markdown(response_content)
+                st.session_state["messages"].append(
+                    {
+                        "role": "assistant",
+                        "content": response_content,
+                        "thinking": thinking_content,
+                    },
+                )
+        add_message(st.session_state["chosen_session"], st.session_state["messages"])
